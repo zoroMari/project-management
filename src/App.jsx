447ada6f7ef1553f5fee389c57a1679d40ba9d7f
Mariia Moroz <mashanya.kov@gmail.com>
2023-11-07 11:34:29 +0100

    added select and delete possibilities

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Sidebar from './components/Sidebar';
 import NewProject from './components/NewProject';
 import { useState } from 'react';
 import NoProjectSelected from './components/NoProjectSelected';
+import SelectedProject from './components/SelectedProject';
 
 
 function App() {
@@ -43,7 +44,31 @@ function App() {
     })
   }
 
-  let content;
+  function handleSelectProject(projectId) {
+    setProjectsState((prev) => {
+      return {
+        ...prev, 
+        selectedProjectId: projectId,
+      }
+    });
+  }
+
+  const selectedProject = projectsState.projects.find(item => item.id === projectsState.selectedProjectId);
+
+  function handleDeleteProject() {
+    setProjectsState((prev) => {
+      return {
+        ...prev,
+        selectedProjectId: undefined,
+        projects: prev.projects.filter((item) => item.id !== projectsState.selectedProjectId),
+      }
+    })
+  }
+
+  let content = <SelectedProject 
+    project={selectedProject} 
+    onDeleteProject={handleDeleteProject}
+  />
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onSaveNewProject={handleSaveNewProject} onCancelNewProject={handleCancelNewPr} />;
@@ -53,7 +78,12 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar projects={projectsState.projects} onStartAddProject={handleStartAddProject}/>
+      <Sidebar 
+        projects={projectsState.projects} 
+        onStartAddProject={handleStartAddProject}
+        onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
+      />
       {content}
     </main>
   );
