7aa258253985882a7e062e78511d97d253ed599f
Mariia Moroz <98117895+zoroMari@users.noreply.github.com>
2023-11-07 11:35:46 +0100

    Select projects (#1)
    added select and delete possibilities

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,5 @@
 import { forwardRef } from "react";
 
-
 const Input = forwardRef(function Input({textarea, label, ...props}, ref) {
   const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-100 text-stone-600 focus:outline-none focus:border-stone-600";
   
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import Button from './Button';
 
-export default function Sidebar({projects, onStartAddProject}) {
+export default function Sidebar({ projects, onStartAddProject, onSelectProject, selectedProjectId }) {
   return (
     <aside className="flex flex-col w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="font-bold mb-8 uppercase md: text-xl text-stone-200">
@@ -14,13 +14,26 @@ export default function Sidebar({projects, onStartAddProject}) {
         </Button>
       </div>
       <ul className="mt-8">
-        {projects.map((project) => (
-          <li key={project.id}>
-            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
-              {project.title}
-            </button>
-          </li>
-        ))}
+        {projects.map((project) => {
+          let cssClasses = 'w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800'
+
+          if (project.id === selectedProjectId) {
+            cssClasses += ' bg-stone-800 text-stone-200'
+          } else {
+            cssClasses += ' text-stone-400'
+          }
+
+          return (
+            <li key={project.id}>
+              <button 
+                onClick={() => onSelectProject(project.id)}
+                className={cssClasses}
+              >
+                {project.title}
+              </button>
+            </li>
+          )
+        })}
       </ul>
     </aside>
   )
